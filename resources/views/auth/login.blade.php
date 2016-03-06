1be13fea808eae1152542ad5a843859dfb3cabe8
Arjay Adong <arjayads@gmail.com>
2016-03-06 21:40:58 +0800

    simple template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.simple')
 
 @section('title', 'Login')
 
---
+++ resources/views/layouts/simple.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Klinika | @yield('title')</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- Web Fonts -->
+        <link rel='stylesheet' type='text/css' href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600&amp;subset=cyrillic,latin'>
+
+    <!-- CSS Global Compulsory -->
+    <link rel="stylesheet" href="/css/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app/clinic.css" type="text/css">
+    <link rel="stylesheet" href="/css/unify/custom.css">
+    <link rel="stylesheet" href="/css/unify/style.css">
+    <link rel="stylesheet" href="/css/unify/headers/header-default.css">
+
+        @yield('css')
+</head>
+<body id="app-layout">
+    <div class="wrapper">
+
+        <div class="header">
+            <div class="container">
+                <!-- Logo -->
+                <a class="logo" href="/">
+                    <img src="/images/logo1-default.png" alt="Logo">
+                </a>
+                <!-- End Logo -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="fa fa-bars"></span>
+                </button>
+            </div><!--/end container-->
+            <div style="margin-bottom: 40px"></div>
+        </div>
+        <div class="container content">
+            @yield('content')
+        </div>
+    </div><!--/wrapper-->
+
+
+    <!-- JavaScripts -->
+    <script type="text/javascript" src="/js/jquery/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
+    <script>
+        $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+        });
+    </script>
+
+    <!--[if lt IE 9]>
+    <script src="/js/unify/respond.js"></script>
+    <script src="/js/unify/html5shiv.js"></script>
+    <script src="/js/unify/placeholder-IE-fixes.js"></script>
+    <![endif]-->
+
+    @yield('js')
+</body>
+</html>
