e5a417894e693e63dc0d77984101ea981d2972fb
Arjay Adong <arjayads@gmail.com>
2016-04-16 23:07:21 +0800

    fix tab

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -125,20 +125,23 @@
         App.init();
         setNavigation();
 
-        $('a[href*=#]:not([href=#])').click(function() {
-           if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-                   || location.hostname == this.hostname) {
+        @if(\Illuminate\Support\Facades\Auth::guest())
+            $('a[href*=#]:not([href=#])').click(function() {
+               if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+                       || location.hostname == this.hostname) {
 
-               var target = $(this.hash);
-               target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-               if (target.length) {
-                   $('html,body').animate({
-                       scrollTop: target.offset().top
-                   }, 1000);
-                   return false;
+                   var target = $(this.hash);
+                   target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                   if (target.length) {
+                       $('html,body').animate({
+                           scrollTop: target.offset().top
+                       }, 1000);
+                       return false;
+                   }
                }
-           }
-       });
+           });
+        @endif
+
 
        function setNavigation() {
            var path = window.location.pathname;
